2db416dbc83687b9c14554993d826cb3f966dc16
fatma dogan and jonathan carlos <jonathan@>
2020-01-21 13:11:15 -0800

    refactored javascript with loops.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
 $(document).ready(function() {
   $("#things").submit(function(event) {
-    var input1 = $("input#thing1").val();
-    var input2 = $("input#thing2").val();
-    var input3 = $("input#thing3").val();
-    var input4 = $("input#thing4").val();
-    var favorites1 = [input1, input2, input3, input4]
-    var favorites2 = []
+    var blanks = ["thing1", "thing2", "thing3", "thing4"]
 
-    favorites2.push(input2, input3, input4);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    
-    $(".answer1").text(input2);
-    $(".answer2").text(input3);
-    $(".answer3").text(input4);
-    
     $(".answers").show();
 
     event.preventDefault();
   });
-
-  // $("#result").show(function(event) {
-  //   $(".answer1").text(input2);
-  //   $(".answer2").text(input3);
-  //   $(".answer3").text(input4);
-
-  //   event.preventDefault();
-  // });
-
+  
 });
+
+// $("#things").submit(function(event) {
+  // var input1 = $("input#thing1").val();
+  // var input2 = $("input#thing2").val();
+  // var input3 = $("input#thing3").val();
+  // var input4 = $("input#thing4").val();
+  // var favorites1 = [input1, input2, input3, input4]
+  // var favorites2 = []
+
+  // favorites2.push(input2, input3, input4);
+
+  
+  // $(".thing1").text(input2);
+  // $(".thing2").text(input3);
+  // $(".thing3").text(input4);
+  
+  // $(".answers").show();
+
+  // event.preventDefault();
+// });
